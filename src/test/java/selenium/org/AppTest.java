acb002caf6ff8bc4040d598c995b8796b9ac77bf
iamkunal2k <kunalkumbhare8@gmail.com>
2022-09-16 15:55:45 +0530

    1st

---
+++ src/test/java/selenium/org/AppTest.java
@@ -0,0 +1,34 @@
+package selenium.org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	@Test
+	public void testhelloworld() {
+		System.setProperty("webdriver.chrome.driver", "D:\\W3schools\\JAVA\\eclipse\\chromedriver.exe");
+		// Instantiate a chromedriver class.
+		WebDriver driver = new ChromeDriver();
+		
+		//Launch Website
+		driver.navigate().to("https://www.javatpoint.com/");
+		
+		//Maximize the browser
+		driver.manage().window().maximize();
+		
+		//scroll down the webpage by 5000px
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("scrollBy(0, 5000)");
+		
+		//Click on the search text box and send value
+		driver.findElement(By.id("gsc-i-id1")).sendKeys("core Java");
+		
+		//Click on the Search button
+		driver.findElement(By.className("gsc-search-button")).click();
+	}
+
+	}
+
